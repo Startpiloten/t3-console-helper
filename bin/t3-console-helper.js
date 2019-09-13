70400e024b71091d43827f6a751344792f5ed203
Boris Schauer <me@bschauer.de>
2019-09-13 16:03:19 +0200

    Some Tests

--- bin/t3-console-helper.js
+++ bin/t3-console-helper.js
@@ -5,6 +5,68 @@ const figlet = require('figlet');
 const inquirer = require('inquirer');
 const shell = require('shelljs');
 const clc = require("cli-color");
+const yaml = require('js-yaml');
+const fs = require('fs');
+const writeJsonFile = require('write-json-file');
+
+//
+const config = yaml.load(fs.readFileSync('.t3.sync.yaml', 'utf8'));
+const availableServers = [];
+
+var myChoises = [
+    new inquirer.Separator('-- Cache'),
+    'cache:flush',
+    'cache:flush --force',
+    new inquirer.Separator('-- Database'),
+    'database:updateschema',
+    {
+        name: 'database:export',
+        value: 'database:export > backup.sql'
+    },
+    new inquirer.Separator('-- Install'),
+    'install:fixfolderstructure',
+    'install:extensionsetupifpossible',
+    new inquirer.Separator('-- Language'),
+    'language:update',
+    new inquirer.Separator('-- Sync'),
+    new inquirer.Separator('-- Exit'),
+    'exit'
+];
+
+var myJson = {
+    "name": "typo3-console-helper",
+    "version": "0.1.2",
+    "description": "TYPO3 Console Helper",
+    "author": "Boris Schauer",
+    "license": "GPL-3.0-or-later",
+    "scripts": {
+        "test": "bo"
+    }
+};
+
+// console.log(myJson);
+
+console.log(myJson.scripts);
+
+Object.keys(config).forEach(function (key) {
+    if (key !== 'local') {
+        myChoises.push(key);
+        let obj = {['sync' + key]: key};
+        console.log(obj);
+        Object.assign(myJson.scripts, obj)
+    }
+});
+
+
+writeJsonFile('foo.json', myJson);
+
+
+// console.log(myJson);
+
+// console.log(availableServers);
+// console.log(config);
+
+var myArgs = process.argv.slice(2);
 
 // variables:
 const executePath = process.cwd();
@@ -31,25 +93,7 @@ const consoleCommands = function () {
             type: 'list',
             message: 'Which command do you want to run?',
             pageSize: 20,
-            choices: [
-                new inquirer.Separator('-- Cache'),
-                'cache:flush',
-                'cache:flush --force',
-                new inquirer.Separator('-- Database'),
-                'database:updateschema',
-                {
-                    name: 'database:export',
-                    value: 'database:export > backup.sql'
-                },
-                new inquirer.Separator('-- Install'),
-                'install:fixfolderstructure',
-                'install:extensionsetupifpossible',
-                new inquirer.Separator('-- Language'),
-                'language:update',
-                new inquirer.Separator('-- Exit'),
-                'exit'
-            ],
-
+            choices: myChoises,
             default: 0,
         }]).then((answers) => {
             if (answers.t3console == 'exit') {
@@ -63,20 +107,25 @@ const consoleCommands = function () {
 };
 
 const runCmd = function (command) {
-    shell.exec(command, {silent:true}, function (code, stdout) {
+    shell.exec(command, {silent: true}, function (code, stdout) {
         if (code === 0) {
             console.log(clc.greenBright(stdout));
         } else if (code === 1) {
             console.log(clc.redBright(stdout));
         }
-        consoleCommands();
+        // consoleCommands();
     });
 };
 
 
 const runAll = function () {
     welcomeText();
-    consoleCommands();
+    if (myArgs.length > 0) {
+        const t3cmd = 'php typo3cms ' + myArgs[0];
+        runCmd(t3cmd);
+    } else {
+        // consoleCommands();
+    }
 };
 
 runAll();
